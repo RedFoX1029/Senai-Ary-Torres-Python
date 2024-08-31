1bd821ad6373190098e06175e7a33349dc91c6b0
Levi Nicola <138073857+RedFoX1029@users.noreply.github.com>
2024-08-31 16:19:54 -0300

    Delete Desafio 3.py

--- Desafio 3.py
+++
@@ -1,6 +0,0 @@
-# Program para realizar uma operação matematica
-
-num1 = int(input('Informa o primeiro número: '))
-num2 = int(input('Informe o segundo número: '))
-
-print(f'{num1} + {num2} = {num1 + num2}')
