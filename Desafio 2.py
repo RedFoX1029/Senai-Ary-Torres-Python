0731dbcd83c0bfbb738cf8d3c0bd9110d25550b9
Levi Nicola <138073857+RedFoX1029@users.noreply.github.com>
2024-08-31 16:19:45 -0300

    Delete Desafio 2.py

--- Desafio 2.py
+++
@@ -1,7 +0,0 @@
-# Programa para ler a data de nascimento
-
-dia = input('Informe o dia de seu nascimento: ')
-mes = input('Informe o mês (escreva o nome por extenso): ')
-ano = input('Informe o ano de seu nascimento: ')
-
-print(f'Você nasceu em {dia} de {mes} de {ano}.')
